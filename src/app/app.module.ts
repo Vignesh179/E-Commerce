1b9c86fde6cbe1c59e45066f33b5b8d8235d04d0
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-03-02 12:17:29 +0530

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/view/header/header.component';
+import { FooterComponent } from './components/view/footer/footer.component';
+import { NavComponent } from './components/view/nav/nav.component';
+import { ShoppingComponent } from './components/shopping/shopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ShoppingComponent
   ],
   imports: [
     BrowserModule,
