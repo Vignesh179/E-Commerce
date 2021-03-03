4ce2a032de343572e72c3075482b73eedd808c7c
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-03-03 12:46:08 +0530

    Final Commit

--- src/app/components/shopping/cart/cart.component.ts
+++ src/app/components/shopping/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessengerService } from 'src/app/services/messenger.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  cartItems = [
+    {id:1, productName:'OnePlus', qty:4, price: 400},
+    {id:2, productName:'OnePlus', qty:5, price: 200},
+    {id:3, productName:'OnePlus', qty:2, price: 300},
+    {id:4, productName:'OnePlus', qty:3, price: 500},
+  ];
 
-  constructor() { }
+  cartTotal = 0;
+  constructor(private msg:MessengerService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+
+    this.msg.getMsg().subscribe(product =>{
+      newFunction(product);
+      this.cartItems.forEach(item =>{
+        this.cartTotal+= item.qty * item.price;
+      })
+    })
+
+  
   }
 
 }
+function newFunction(product: unknown) {
+  console.log(product);
+}
+
--- src/app/components/shopping/product-list/product-list.component.ts
+++ src/app/components/shopping/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
