3ad964bde95be9aa3ff82cc471671c52aac4e239
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-03-03 12:52:39 +0530

    Bug

--- src/app/components/shopping/cart/cart.component.ts
+++ src/app/components/shopping/cart/cart.component.ts
@@ -8,10 +8,10 @@ import { MessengerService } from 'src/app/services/messenger.service';
 })
 export class CartComponent implements OnInit {
   cartItems = [
-    {id:1, productName:'OnePlus', qty:4, price: 400},
-    {id:2, productName:'OnePlus', qty:5, price: 200},
-    {id:3, productName:'OnePlus', qty:2, price: 300},
-    {id:4, productName:'OnePlus', qty:3, price: 500},
+    {id:1, productName:'OnePlus', qty:1, price: 1250},
+    {id:2, productName:'OnePlus', qty:1, price: 1250},
+    {id:3, productName:'OnePlus', qty:1, price: 1250},
+    {id:4, productName:'OnePlus', qty:1, price: 1250},
   ];
 
   cartTotal = 0;
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,9 +7,15 @@ import { Product } from 'src/app/models/product'
 export class ProductService {
 products: Product[] = [
   new Product(1,'Product name 1','OnePlus 1', 5000),
-  new Product(2,'Product name 2','OnePlus 2', 9000),
-  new Product(3,'Product name 3','OnePlus 3', 15000),
-  new Product(4,'Product name 4','OnePlus 4', 7000)
+  new Product(2,'Product name 2','OnePlus 2', 5000),
+  new Product(3,'Product name 3','OnePlus 3', 5000),
+  new Product(5,'Product name 5','OnePlus 5', 5000),
+  new Product(6,'Product name 6','OnePlus 6', 5000),
+  new Product(7,'Product name 7','OnePlus 7', 5000),
+  new Product(8,'Product name 8','OnePlus 8', 5000),
+  new Product(9,'Product name 9','OnePlus 9', 5000),
+  new Product(10,'Product name 10','OnePlus 10', 5000),
+  new Product(4,'Product name 4','OnePlus 4', 5000)
 ]
   constructor() { }
 
